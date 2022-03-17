661858af940cb587f051b2ac06c33f45edd10966
MiguelArdon <menriquez@crc.global>
2022-03-17 11:14:42 -0600

    generating the download file csv, using one date or a range of dates

--- routes/inbounds.js
+++ routes/inbounds.js
@@ -1,29 +1,71 @@
 var express = require("express");
 var router = express.Router();
+const csv = require('fast-csv');
 const inbounds = require('../services/inbounds');
 
 router.get("/", async function (request, response) {
+  response.setHeader('content-type', 'text/csv');
+  var csvStream = csv.format({headers: ['ID','Date','Customer','Warehouse','BOL','ArriveTime','ScheduleTime','ArriveStatus','DepartureTime','OnSiteTime','CreationDate','Carrier','Brand'], objectMode: true});
+  csvStream.pipe(response);  
   if(request.query.date){
-    try{
-      const answer = await inbounds.getInboundByDate(request.query.date);
-      return response.status(200).json(
-        answer
-        );
-    }catch(error){
+    response.setHeader('Content-disposition', `attachment; filename=InboundReport${request.query.date}.csv`);
+      try{
+        const answer = await inbounds.getInboundByDate(request.query.date,);
+
+        answer.forEach((array) =>{
+          array.inbounds.forEach((inbound) =>{
+            csvStream.write({
+              'ID':inbound.id,
+              'Date':inbound.date,
+              'Customer':inbound.customer,
+              'Warehouse':inbound.facilityName,
+              'BOL':inbound.bol,
+              'ArriveTime':inbound.arrivalTime,
+              'ScheduleTime':inbound.schedule,
+              'ArriveStatus':inbound.arrival,
+              'DepartureTime':inbound.depart,
+              'OnSiteTime':inbound.siteTime,
+              'CreationDate':inbound.creationDate,
+              'Carrier':inbound.carrier,
+              'Brand':inbound.brand
+            });
+          })
+        })
+
+      }catch(error){
+        return response.status(500).json({
+          message: error.message
+        });
+      }
+    }else if(request.query.initDate && request.query.finalDate){
+    response.setHeader('Content-disposition', `attachment; filename=InboundReport${request.query.initDate}-${request.query.finalDate}.csv`);
+      const answerRange = await inbounds.getInboundByDate(request.query.initDate, request.query.finalDate);
+      answerRange.forEach((array) =>{
+        array.inbounds.forEach((inbound) =>{
+          csvStream.write({
+            'ID':inbound.id,
+            'Date':inbound.date,
+            'Customer':inbound.customer,
+            'Warehouse':inbound.facilityName,
+            'BOL':inbound.bol,
+            'ArriveTime':inbound.arrivalTime,
+            'ScheduleTime':inbound.schedule,
+            'ArriveStatus':inbound.arrival,
+            'DepartureTime':inbound.depart,
+            'OnSiteTime':inbound.siteTime,
+            'CreationDate':inbound.creationDate,
+            'Carrier':inbound.carrier,
+            'Brand':inbound.brand
+          });
+        })
+      })
+    }else{
       return response.status(500).json({
-        message: error.message
-      });
+        message: "No Date input!"
+      })
     }
-  }else if(request.query.initDate && request.query.finalDate){
-    const answerRange = await inbounds.getInboundByDate(request.query.initDate, request.query.finalDate);
-      return response.status(200).json(
-        answerRange
-        );
-  }else{
-    return response.status(500).json({
-      message: "No Date input!"
-    })
-  }
+    csvStream.end();
+  
 });
 
 module.exports = router;
--- services/inbounds.js
+++ services/inbounds.js
@@ -4,132 +4,123 @@ const moment = require("moment");
 const e = require("express");
 
 async function getInboundByDate(initDate, finalDate) {
-    var rows;
-    var data;
-    if (finalDate) {
-        rows = await db.query(
-            `select * from schedule inner Join warehouses on tplfacility = facilityId where direction =  'Inbound' and scheduledate Between ${initDate} and ${finalDate}`
-        );
-        data = helper.emptyOrRows(rows);
-    } else {
-        rows = await db.query(
-            `select * from schedule inner Join warehouses on tplfacility = facilityId where direction =  'Inbound' and scheduledate = ${initDate}`
-        );
-        data = helper.emptyOrRows(rows);
+  var rows;
+  var data;
 
+  if (finalDate) {
+    rows = await db.query(
+      `select * from schedule inner Join warehouses on tplfacility = facilityId where direction =  'Inbound' and scheduledate Between ${initDate} and ${finalDate} order by facilityid`
+    );
+    data = helper.emptyOrRows(rows);
+  } else {
+    rows = await db.query(
+      `select * from schedule inner Join warehouses on tplfacility = facilityId where direction =  'Inbound'  and scheduledate = ${initDate} order by facilityid`
+    );
+    data = helper.emptyOrRows(rows);
+  }
+  var preparedData = [];
+  var structuredInbound;
+  var finalInbounds = [];
+
+
+  var status;
+  var arrive;
+  var depart;
+  var creationDate;
+  var schedule;
+  var date;
+  var scheduleWindowLate;
+  var scheduleWindowEarly;
+  var differenceTimeLate;
+  var differenceTimeEarly;
+  var earlyIsBefore;
+
+  data.forEach((inbound) => {
+    depart = moment(inbound.departedts).format("LTS");
+    date = moment(inbound.scheduledate).format("YYYY-MM-DD");
+    creationDate = moment(inbound.dtcreated).format("YYYY-MM-DD h:m:s a");
+    var duration;
+    var formatDuration;
+    
+    if (inbound.arrivedts) {
+      arrive = moment(inbound.arrivedts).format("LTS");
+      schedule = moment(inbound.scheduletime, "LTS").format("LTS");
+
+      scheduleWindowLate = moment(schedule, "LTS")
+        .add(30, "minutes")
+        .format("LTS");
+      scheduleWindowEarly = moment(schedule, "LTS")
+        .subtract(30, "minutes")
+        .format("LTS");
+
+      differenceTimeLate = moment(arrive, "LTS").isBetween(
+        moment(schedule, "LTS"),
+        moment(scheduleWindowLate, "LTS")
+      );
+      differenceTimeEarly = moment(arrive, "LTS").isBetween(
+        moment(scheduleWindowEarly, "LTS"),
+        moment(schedule, "LTS")
+      );
+      earlyIsBefore = moment(arrive, "LTS").isBefore(moment(schedule, "LTS"));
+
+      if (
+        differenceTimeLate == true &&
+        differenceTimeEarly == false &&
+        earlyIsBefore == false
+      ) {
+        status = "OnTime";
+      } else if (
+        differenceTimeLate == false &&
+        differenceTimeEarly == true &&
+        earlyIsBefore == false
+      ) {
+        status = "Early";
+      } else if (
+        differenceTimeLate == true &&
+        differenceTimeEarly == true &&
+        earlyIsBefore == false
+      ) {
+        status = "OnTime";
+      } else if (
+        differenceTimeLate == false &&
+        differenceTimeEarly == false &&
+        earlyIsBefore == true
+      ) {
+        status = "Early";
+      } else if (
+        differenceTimeLate == false &&
+        differenceTimeEarly == false &&
+        earlyIsBefore == false
+      ) {
+        status = "Late";
+      }
+      duration = moment.duration(moment(inbound.departedts).diff(moment(inbound.arrivedts)));
+      formatDuration = Math.floor(duration.asHours()) + "h " + Math.floor(duration.minutes()) + "m ";
+    } else if (inbound.arrivedts == null) {
+      status = "No Show";
+      formatDuration = null;
+      depart = null;
     }
 
-    var preparedData = [];
-    var structuredInbound;
-    var structurePendingInbounds;
-    var finalInbounds = [];
-    var finalPending = [];
-
-    var status;
-    var arrive;
-    var depart;
-    var schedule;
-    var date;
-    var scheduleWindowLate;
-    var scheduleWindowEarly;
-    var differenceTimeLate;
-    var differenceTimeEarly;
-    var earlyIsBefore;
-
-
-    data.forEach((inbound) => {
-        depart = moment(inbound.departedts).format("LTS");
-        date = moment(inbound.scheduledate).format("YYYY-MM-DD");
-
-        if (inbound.currentstate == "COMPLETE") {
-            if (inbound.arrivedts) {
-                arrive = moment(inbound.arrivedts).format("LTS");
-                schedule = moment(inbound.scheduletime, "LTS").format("LTS");
-
-                scheduleWindowLate = moment(schedule, "LTS")
-                    .add(30, "minutes")
-                    .format("LTS");
-                scheduleWindowEarly = moment(schedule, "LTS")
-                    .subtract(30, "minutes")
-                    .format("LTS");
-
-                differenceTimeLate = moment(arrive, "LTS").isBetween(
-                    moment(schedule, "LTS"),
-                    moment(scheduleWindowLate, "LTS")
-                );
-                differenceTimeEarly = moment(arrive, "LTS").isBetween(
-                    moment(scheduleWindowEarly, "LTS"),
-                    moment(schedule, "LTS")
-                );
-                earlyIsBefore = moment(arrive, "LTS").isBefore(moment(schedule, "LTS"));
-
-                if (
-                    differenceTimeLate == true &&
-                    differenceTimeEarly == false &&
-                    earlyIsBefore == false
-                ) {
-                    status = "OnTime";
-                } else if (
-                    differenceTimeLate == false &&
-                    differenceTimeEarly == true &&
-                    earlyIsBefore == false
-                ) {
-                    status = "Early";
-                } else if (
-                    differenceTimeLate == true &&
-                    differenceTimeEarly == true &&
-                    earlyIsBefore == false
-                ) {
-                    status = "OnTime";
-                } else if (
-                    differenceTimeLate == false &&
-                    differenceTimeEarly == false &&
-                    earlyIsBefore == true
-                ) {
-                    status = "Early";
-                } else if (
-                    differenceTimeLate == false &&
-                    differenceTimeEarly == false &&
-                    earlyIsBefore == false
-                ) {
-                    status = "Late";
-                }
-            } else if (inbound.arrivedts == null) {
-                status = "No Show";
-            }
-
-            var duration = moment.duration(moment(inbound.departedts).diff(moment(inbound.arrivedts)));
-            var formatDuration = Math.floor(duration.asHours()) + 'h ' + Math.floor(duration.minutes()) + 'm ';
-
-            structuredInbound = {
-                id: inbound.id,
-                date: date,
-                customer: inbound.customer,
-                facilityName: inbound.facilityName || null,
-                bol: inbound.bol,
-                arrivalTime: arrive || null,
-                schedule: schedule || null,
-                arrival: status,
-                depart: depart || null,
-                siteTime: formatDuration,
-                creationDate: inbound.creationDate,
-                carrier: inbound.carrier,
-                brand: inbound.brand,
-            };
-            finalInbounds.push(structuredInbound);
-        } else if (inbound.currentstate == "SCHEDULED") {
-            structurePendingInbounds = {
-                id: inbound.id,
-                date: inbound.scheduledate,
-                customer: inbound.customer,
-                facilityName: inbound.facilityName,
-                bol: inbound.bol,
-            }
-            finalPending.push(structurePendingInbounds);
-        }
-    });
-    preparedData.push({ inbounds: finalInbounds }, { pending: finalPending });
-    return preparedData;
+    structuredInbound = {
+      id: inbound.id,
+      date: date,
+      customer: inbound.customer,
+      facilityName: inbound.facilityName || null,
+      bol: inbound.bol,
+      arrivalTime: arrive || null,
+      schedule: schedule || null,
+      arrival: status,
+      depart: depart || null,
+      siteTime: formatDuration,
+      creationDate: creationDate || null,
+      carrier: inbound.carrier,
+      brand: inbound.brand,
+    };
+    finalInbounds.push(structuredInbound);
+  });
+  preparedData.push({ inbounds: finalInbounds });
+  return preparedData;
 }
 
 module.exports = { getInboundByDate };
